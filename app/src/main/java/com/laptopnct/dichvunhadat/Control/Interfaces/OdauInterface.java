c80e22b8557bcdcee0feea26923e219ef429298b
000HelloWorld000 <43272053+000HelloWorld000@users.noreply.github.com>
2020-06-15 21:33:20 +0700

    first commit

---
+++ app/src/main/java/com/laptopnct/dichvunhadat/Control/Interfaces/OdauInterface.java
@@ -0,0 +1,11 @@
+package com.laptopnct.dichvunhadat.Control.Interfaces;
+
+import com.laptopnct.dichvunhadat.Model.CongTyModel;
+
+/**
+ * Created by Cong Thanh
+ */
+
+public interface OdauInterface {
+    void getDanhSachCongTyModel(CongTyModel congTyModel);
+}
